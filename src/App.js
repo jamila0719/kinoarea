db5a431379a212ee383880e92320f134b76ab029
Jamila <jamila19ravshanova@gmail.com>
2024-04-30 19:33:59 +0500

    error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import Header from './components/sections/header/header';
+import RouterCom from './routes';
 
 export default function App() {
   return <Box>
       <Header />
-      
+      <RouterCom />
     </Box>;
   
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
+import NotFound from '../components/pages/PageNotFound/notFound';
+
 export default function RouterCom () {
     return( 
     <Routes>
-        {/* <Route path="" element={} />
-        <Route path="*" element={} /> */}
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
     </Routes>
 
     );
